24ee5d48e071daf6f01c8e89f3d9eb2c23facb0a
saulasciante <ms1004@student.uni-lj.si>
2021-03-05 18:08:34 +0100

    added main script

---
+++ opticalFlow.py
@@ -0,0 +1,8 @@
+
+
+def lucasKanade(img1, img2, N):
+    # img1 - first image matrix (grayscale)
+    # img2 - second image matrix (grayscale)
+    # n - size of the neighbourhood (N x N)
+
+    return
